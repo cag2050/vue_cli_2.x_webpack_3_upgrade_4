e55191117e7139f8279ebe4dfb11baf1f1182ef3
chen_anguo <cag2050@163.com>
2019-01-15 19:03:07 +0800

    添加功能:上线后程序刷新一次浏览器，来使用新上线资源

--- build/build.js
+++ build/build.js
@@ -10,32 +10,45 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
+const fs = require('fs')
 
 const spinner = ora('building for production...')
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-    if (err) throw err
-    webpack(webpackConfig, (err, stats) => {
-        spinner.stop()
-        if (err) throw err
-        process.stdout.write(stats.toString({
-            colors: true,
-            modules: false,
-            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n')
-
-        if (stats.hasErrors()) {
-            console.log(chalk.red('  Build failed with errors.\n'))
-            process.exit(1)
-        }
-
-        console.log(chalk.cyan('  Build complete.\n'))
-        console.log(chalk.yellow(
-            '  Tip: built files are meant to be served over an HTTP server.\n' +
-            '  Opening index.html over file:// won\'t work.\n'
-        ))
-    })
+// 将当前时间戳写入json文件
+let json_obj = {"build_str": new Date().getTime().toString()}
+fs.writeFile(path.resolve(__dirname, '../static/json/build_str.json'), JSON.stringify(json_obj), function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log("打包字符串写入文件：static/json/build_str.json，成功！");
+    realBuild()
 })
+
+function realBuild () {
+    rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+        if (err) throw err
+        webpack(webpackConfig, (err, stats) => {
+            spinner.stop()
+            if (err) throw err
+            process.stdout.write(stats.toString({
+                colors: true,
+                modules: false,
+                children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+                chunks: false,
+                chunkModules: false
+            }) + '\n\n')
+
+            if (stats.hasErrors()) {
+                console.log(chalk.red('  Build failed with errors.\n'))
+                process.exit(1)
+            }
+
+            console.log(chalk.cyan('  Build complete.\n'))
+            console.log(chalk.yellow(
+                '  Tip: built files are meant to be served over an HTTP server.\n' +
+                '  Opening index.html over file:// won\'t work.\n'
+            ))
+        })
+    })
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+    axios.get('/static/json/build_str.json?v=' + new Date().getTime().toString())
+        .then(res => {
+            let newBuildStr = res.data.build_str
+            let oldBuildStr = localStorage.getItem('build_str') || ''
+            if (oldBuildStr !== newBuildStr) {
+                console.log('auto refresh')
+                localStorage.setItem('build_str', newBuildStr)
+                location.reload()
+            }
+        })
+    next()
+})
 
 /* eslint-disable no-new */
 new Vue({
